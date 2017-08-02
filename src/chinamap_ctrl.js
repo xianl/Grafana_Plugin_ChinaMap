343b3ae657c3a5b3fbcd9a8c7d193af7c4a3cb29
xianl <xianl@microsoft.com>
2017-08-02 22:56:40 +0800

    1

--- src/chinamap_ctrl.js
+++ src/chinamap_ctrl.js
@@ -21,20 +21,31 @@ link(scope, elem, attrs, ctrl) {
 
 		
 		
-         var option = {
-			title: { text: 'ECharts 入门示例' },
-			tooltip: {},
-			xAxis: {
-				data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+         option = {
+			tooltip: {
+				trigger: 'item',
+				formatter: '{b}'
 			},
-			yAxis: {},
-			series: [{
-				name: '销量',
-				type: 'bar',
-				data: [5, 20, 36, 10, 10, 20]
-			}]
+			series: [
+				{
+					name: '中国',
+					type: 'map',
+					mapType: 'china',
+					selectedMode : 'multiple',
+					label: {
+						normal: {
+							show: true
+						},
+						emphasis: {
+							show: true
+						}
+					},
+					data:[
+						{name:'广东', selected:true}
+					]
+				}
+			]
 		};
-
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
 		console.log("finish");
