efa908a8820c1c96bfedc532eefca2342418fe5f
xianl <xianl@microsoft.com>
2017-08-03 10:36:05 +0800

    1

--- dist/map_renderer.js
+++ dist/map_renderer.js
@@ -1,43 +1,67 @@
 'use strict';
 
 System.register(['./lib/echarts.min'], function (_export, _context) {
-  "use strict";
+	"use strict";
 
-  var echarts;
-  function link(scope, elem, attrs, ctrl) {
-    var mapContainer = elem.find('.chinamap-panel');
+	var echarts;
+	function link(scope, elem, attrs, ctrl) {
+		var mapContainer = elem.find('.chinamap-panel');
 
-    ctrl.events.on('render', function () {
-      render();
-      ctrl.renderingCompleted();
-    });
+		ctrl.events.on('render', function () {
+			render();
+			ctrl.renderingCompleted();
+		});
 
-    function render() {
-      //if (!ctrl.data) return;
+		function render() {
+			//if (!ctrl.data) return;
 
-      if (!ctrl.map) {
-        var myChart = echarts.init(mapContainer[0]);
-      }
+			if (!ctrl.map) {
+				var myChart = echarts.init(mapContainer[0]);
+			}
 
-      /*
-         ctrl.map.resize();
-      
-         if (ctrl.mapCenterMoved) ctrl.map.panToMapCenter();
-      
-         if (!ctrl.map.legend && ctrl.panel.showLegend) ctrl.map.createLegend();
-      
-         ctrl.map.drawCircles();
-      */
-    }
-  }
+			var option = {
+				tooltip: {
+					trigger: 'item',
+					formatter: '{b}'
+				},
+				series: [{
+					name: '中国',
+					type: 'map',
+					mapType: 'china',
+					selectedMode: 'multiple',
+					label: {
+						normal: {
+							show: true
+						},
+						emphasis: {
+							show: true
+						}
+					},
+					data: [{ name: '广东', selected: true }]
+				}]
+			};
+			// 使用刚指定的配置项和数据显示图表。
+			myChart.setOption(option);
+			console.log("finish");
+			/*
+      ctrl.map.resize();
+   
+      if (ctrl.mapCenterMoved) ctrl.map.panToMapCenter();
+   
+      if (!ctrl.map.legend && ctrl.panel.showLegend) ctrl.map.createLegend();
+   
+      ctrl.map.drawCircles();
+   */
+		}
+	}
 
-  _export('default', link);
+	_export('default', link);
 
-  return {
-    setters: [function (_libEchartsMin) {
-      echarts = _libEchartsMin.default;
-    }],
-    execute: function () {}
-  };
+	return {
+		setters: [function (_libEchartsMin) {
+			echarts = _libEchartsMin.default;
+		}],
+		execute: function () {}
+	};
 });
 //# sourceMappingURL=map_renderer.js.map
--- src/map_renderer.js
+++ src/map_renderer.js
@@ -15,6 +15,35 @@ export default function link(scope, elem, attrs, ctrl) {
       var myChart = echarts.init(mapContainer[0]);
     }
 
+	
+	var option = {
+			tooltip: {
+				trigger: 'item',
+				formatter: '{b}'
+			},
+			series: [
+				{
+					name: '中国',
+					type: 'map',
+					mapType: 'china',
+					selectedMode : 'multiple',
+					label: {
+						normal: {
+							show: true
+						},
+						emphasis: {
+							show: true
+						}
+					},
+					data:[
+						{name:'广东', selected:true}
+					]
+				}
+			]
+		};
+        // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+	console.log("finish");
 	/*
     ctrl.map.resize();
 
