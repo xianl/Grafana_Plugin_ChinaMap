bb5d03a7512360e647ee84813a9e4960c6be284c
xianl <xianl@microsoft.com>
2017-08-04 12:03:53 +0800

    1

--- src/map_renderer.js
+++ src/map_renderer.js
@@ -37,7 +37,9 @@ export default function link(scope, elem, attrs, ctrl) {
 			min: 0,
 			max: 10,
 			left: 'left',
-			color: ctrl.panel.dataColors,
+			inRange: {
+				color: ctrl.panel.dataColors
+			},
 			top: 'bottom',
 			text: ['高','低'],           // 文本，默认为数值文本
 			calculable: true
