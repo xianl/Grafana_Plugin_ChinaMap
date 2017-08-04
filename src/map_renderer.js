36f92a1372dcc322d052a630136df0a6781dc281
xianl <xianl@microsoft.com>
2017-08-04 14:58:04 +0800

    1

--- src/map_renderer.js
+++ src/map_renderer.js
@@ -91,7 +91,7 @@ export default function link(scope, elem, attrs, ctrl) {
     // 使用刚指定的配置项和数据显示图表。
     ctrl.map.setOption(option);
 	
-	console.log(mychart.getHeight());
+	console.log(ctrl.map.getHeight());
     ctrl.map.resize();
 	
 		
