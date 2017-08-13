7b93602599202e8ff110f2fead6e21465121ee0a
xianl <xianl@microsoft.com>
2017-08-13 15:05:00 +0800

    1

--- src/map_renderer.js
+++ src/map_renderer.js
@@ -27,9 +27,9 @@ export default function link(scope, elem, attrs, ctrl) {
 	
 	data.forEach((dataPoint) => {
       
-	  if(dataPoint.key == '天津')  ctrl.mapValue.天津=dataPoint.valueFormatted;
+	  if(dataPoint.key == '天津')  ctrl.mapValue.tianjing=dataPoint.valueFormatted;
 	  //if(dataPoint.key == '辽宁')  ctrl.mapValue.辽宁=dataPoint.valueFormatted;
-	  if(dataPoint.key == '山东')  ctrl.mapValue.山东=dataPoint.valueFormatted;
+	  if(dataPoint.key == '山东')  ctrl.mapValue.shandong=dataPoint.valueFormatted;
 	
 	  
     });
